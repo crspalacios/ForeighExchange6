a47ca37b786eeb3bd1136f57b29b956e63b9d3e6
crspalacios <cristian.palacios.mosquera@gmail.com>
2017-09-20 20:35:04 -0700

    Binding

---
+++ ForeighExchange6/ForeighExchange6/Infrastructure/IntanceLocator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeighExchange6.Infrastructure
+{
+    using ViewModels;
+    public class IntanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public IntanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+    }
+}
---
+++ ForeighExchange6/ForeighExchange6/ViewModels/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeighExchange6.ViewModels
+{
+    using System.Collections.ObjectModel;
+    using Models;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+
+    public class MainViewModel
+    {
+        #region Porperties
+        public String Amount
+        {
+            get;
+            set;
+        }
+        public ObservableCollection<Rate> Rates
+        {
+            get;
+            set;
+        }
+        public Rate SourceRate
+        {
+            get;
+            set;
+        }
+        public Rate TargetRate
+        {
+            get;
+            set;
+        }
+
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+        public bool iIsEnabled
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand ConvertCommand
+        {
+            get
+            {
+                return new RelayCommand (Converts);
+            }
+        }
+
+        private void Converts()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
